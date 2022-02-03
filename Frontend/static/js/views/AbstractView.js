c4169a6d36b7a1ad3ed77255f9fd6f59a29eb01a
dedku <szabat.patryk@gmail.com>
2022-02-03 22:13:42 +0100

    Add sidebar to views

--- Frontend/static/js/views/AbstractView.js
+++ Frontend/static/js/views/AbstractView.js
@@ -8,6 +8,17 @@ export default class {
         document.title = title;
     }
 
+    async getSidebar() {
+            return`
+            <nav class="nav">
+                <a href="/" class="nav__link" data-link="">Dashboard</a>
+                <a href="/invoices" class="nav__link" data-link="">Invoices</a>
+                <a href="/profile" class="nav__link" data-link="">Profile</a>
+                <a href="/appointment" class="nav__link" data-link="">Appointment</a>
+            </nav>
+            `
+    }
+
     async getHtml() {
         return "";
     }
