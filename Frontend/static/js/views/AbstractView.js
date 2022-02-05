8ca12bc4d2b5bc56a09453d16ce0a890841a51db
dedku <szabat.patryk@gmail.com>
2022-02-05 19:42:02 +0100

    hover on link and icons

--- Frontend/static/js/views/AbstractView.js
+++ Frontend/static/js/views/AbstractView.js
@@ -12,54 +12,54 @@ export default class {
     async getSidebar() {
             return`
                 <div class="sidebar">
-                    <div class="sidebar__sidebar-content">
-                        <div class="sidebar__sidebar-content__logo_content">
-                            <div class="sidebar__sidebar-content__logo_content__logo">
+                    <div class="sidebar__content">
+                        <div class="sidebar__logo-content">
+                            <div class="sidebar__logo">
                             <i class='bx bx-plus-medical' ></i>
-                            <div class="sideba__sidebar-contentr__logo_content__logo__logo_name">MedCure</div>
+                            <div class="sideba__logo_text">MedCure</div>
                             </div>
                         </div>
-                        <box-icon name='menu' color='#fff'></box-icon>
-                        <ul class="sidebar__sidebar-content__nav">
-                            <li class="sidebar__sidebar-content__nav__item">
-                                    <i class="bx bx-search-alt-2 sidebar__sidebar-content__nav__item__icon"      color='#fff'></i>
+                        <i class='bx bx-menu sidebar__icon bx-border' ></i>
+                        <ul class="sidebar__nav">
+                            <li class="sidebar__nav-item">
+                                    <i class="bx bx-search-alt-2 sidebar__nav-icon bx-border"      color='#fff'></i>
                                     <input type="text" placeholder="Search...">
                             </li>
-                            <li class="sidebar__sidebar-content__nav__item">
-                                <a class="sidebar__sidebar-content__nav__item__link" href="/" data-link>
-                                    <i class='bx bx-menu' ></i>
-                                    <span class="nav__sidebar-content__link_name">Dashboard</span>
+                            <li class="sidebar__nav-item">
+                                <a class="sidebar__nav-link" href="/" data-link>
+                                    <i class='bx bx-menu sidebar__nav-icon bx-border' ></i>
+                                    <span class="sidebar__link-name">Dashboard</span>
                                 </a>
                             </li>
-                            <li class="sidebar__sidebar-content__nav__item">
-                                <a class="sidebar__sidebar-content__nav__item__link" href="/invoices" data-link>
-                                    <i class='bx bx-book-content' ></i>
-                                    <span class="nav__sidebar-content__link_name">Invoices</span>
+                            <li class="sidebar__nav-item">
+                                <a class="sidebar__nav-link" href="/invoices" data-link>
+                                        <i class='bx bx-book-content bx-border sidebar__nav-icon' ></i>
+                                    <span class="sidebar__link-name">Invoices</span>
                                 </a>
                             </li>
-                            <li class="sidebar__sidebar-content__nav__item">
-                                <a class="sidebar__sidebar-content__nav__item__link" href="/profile" data-link>
-                                    <i class='bx bx-face' ></i>
-                                    <span class="nav__sidebar-content__link_name">Profile</span>
+                            <li class="sidebar__nav-item">
+                                <a class="sidebar__nav-link" href="/profile" data-link>
+                                    <i class='bx bx-face sidebar__nav-icon bx-border' ></i>
+                                    <span class="sidebar__link-name">Profile</span>
                                 </a>
                             </li>
-                            <li class="sidebar__sidebar-content__nav__item">
-                                <a class="sidebar__sidebar-content__nav__item__link" href="/settings" data-link>
-                                    <i class='bx bx-brightness' ></i>
-                                    <span class="nav__sidebar-content__link_name">Settings</span>
+                            <li class="sidebar__nav-item">
+                                <a class="sidebar__nav-link" href="/settings" data-link>
+                                    <i class='bx bx-brightness sidebar__nav-icon bx-border' ></i>
+                                    <span class="sidebar__link-name">Settings</span>
                                 </a>
                             </li>
-                            <li class="sidebar__sidebar-content__nav__item">
-                                <a class="sidebar__sidebar-content__nav__item__link" href="#" data-link>
-                                    <i class='bx bx-brightness' ></i>
-                                    <span class="nav__sidebar-content__link_name">Log out</span>
+                            <li class="sidebar__nav-item">
+                                <a class="sidebar__nav-link" href="#" data-link>
+                                    <i class='bx bx-brightness sidebar__nav-icon bx-border' ></i>
+                                    <span class="sidebar__link-name">Log out</span>
                                 </a>
                             </li>
                         </ul>
-                        <div class="sidebar__sidebar-content__appointment">
-                            <div class="sidebar__sidebar-content__appointment__box">
-                                <div class="sidebar__sidebar-content__appointment__box-button">
-                                    <button> <a href="/appointment" data-link="">Set Appointment </a></button>
+                        <div class="sidebar__appointment">
+                            <div class="sidebar__appointment-box">
+                                <div class="sidebar__appointment-button">
+                                    <button href="/appointment" data-link="">Set Appointment </button>
                                 </div>
                             </div>
                         </div>
