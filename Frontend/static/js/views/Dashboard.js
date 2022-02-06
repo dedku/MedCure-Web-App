fb5162bbc5e74d4b2fed1142c733684254d80588
hose347 <hosse.css@gmail.com>
2022-02-06 20:12:13 +0100

    details to top boxes

--- Frontend/static/js/views/Dashboard.js
+++ Frontend/static/js/views/Dashboard.js
@@ -10,31 +10,36 @@ export default class extends AbstractView {
         return `
     <div class="dashboard">
         <div class="dashboard__box">
-        <p class="dashboard__box__text-top">Todays Money</p>
-        <p class="dashboard__box__text-bottom">$53000</p>
+        <p class="dashboard__box__text-top">New Confirmed</p>
+        <p class="dashboard__box__text-bottom">100350</p>
+        <i class="bx bxs-virus dashboard__box__icon bx-border"></i>
         </div>
 
         <div class="dashboard__box">
-        <p class="dashboard__box__text-top">Todays Users</p>
-        <p class="dashboard__box__text-bottom">2300</p>
+        <p class="dashboard__box__text-top">Total Confirmed</p>
+        <p class="dashboard__box__text-bottom">340567890</p>
+        <i class="bx bx-clinic dashboard__box__icon bx-border" ></i>
         </div>
 
         <div class="dashboard__box">
-        <p class="dashboard__box__text-top">New Clients</p>
-        <p class="dashboard__box__text-bottom">+3052</p>
+        <p class="dashboard__box__text-top">New Deaths</p>
+        <p class="dashboard__box__text-bottom">31052</p>
+        <i class="bx bxs-clinic dashboard__box__icon bx-border"></i>
         </div>
 
         <div class="dashboard__box">
-        <p class="dashboard__box__text-top">Total Sales</p>
-        <p class="dashboard__box__text-bottom">$173000</p>
+        <p class="dashboard__box__text-top">New Recovered</p>
+        <p class="dashboard__box__text-bottom">157890</p>
+        <i class="bx bx-heart dashboard__box__icon bx-border"></i>
         </div>
     </div>
 
     <div class="grid">
         
             <div class="grid__mid-box__1">
-            <h1>Who we are ?</h1>
-            <p>Same koxy, nic wspolnego z medycyną</p>
+            <h1 class="grid__about-h1">Who we are ?</h1>
+            <p class="grid__about-p">MedCure is a medical application that provides services to individual and corporate clinics. 
+            We offer a wide range of medical services, medical consultations, specialist diagnostics and vaccinations.</p>
             </div>
             <div class="grid__mid-box__2">New Boxes</div>
             <div class="grid__mid-box__3">New Boxes</div>
